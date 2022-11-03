fb162cc279767773d04f64d5790b9bb3bff9bb23
mmarques291 <mmarques291@gmail.com>
2022-11-03 03:26:00 -0300

    deleted unused utils.h functions

--- components/utils/include/utils.h
+++ components/utils/include/utils.h
@@ -2,16 +2,7 @@
 #define UTILS_H_LIB
 
 #include <stdint.h>
-#include <string.h>
-
-extern int16_t max_value;
-extern int16_t min_value;
 
 float rms(int16_t *y, int numel);
-int16_t max(int16_t vet[], uint16_t N);
-int16_t min(int16_t vet[], uint16_t N);
-
-void reset_maxmin_values(void);
-void reset_buffer(int16_t* buffer_obj, uint16_t buffer_size);
 
 #endif
--- components/utils/utils.c
+++ components/utils/utils.c
@@ -4,9 +4,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-int16_t max_value = INT16_MIN;
-int16_t min_value = INT16_MAX;
-
 float
 rms(int16_t *function, int total_samples) {
     float integral_squared = 0.0f, y_rms = 0.0f;
@@ -23,39 +20,4 @@ rms(int16_t *function, int total_samples) {
 	y_rms = sqrt((1.0f / total_samples) * integral_squared);
 
 	return y_rms;
-}
-
-int16_t
-max(int16_t vet[], uint16_t N)
-{
-    uint16_t i;
-
-    for (i = 1; i < N; i++)
-        if (vet[i] > max_value)
-            max_value = vet[i];
-    return max_value;
-}
-
-int16_t
-min(int16_t vet[], uint16_t N)
-{
-    uint16_t i;
-
-    for (i = 1; i < N; i++)
-        if (vet[i] < min_value)
-            min_value = vet[i];
-    return min_value;
-}
-
-void
-reset_maxmin_values()
-{
-	max_value = INT16_MIN;
-	min_value = INT16_MAX;
-}
-
-void
-reset_buffer(int16_t* buffer_obj, uint16_t buffer_size)
-{
-	memset(buffer_obj, 0, buffer_size);
 }
